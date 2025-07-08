98b890df424f5c70c904a479c2ce818e5eabfb84
yunjong <haydenkuk@gmail.com>
2025-07-08 19:00:16 -0400

    num_regex error fixed. the regex matched hypen in the middle before

--- gsheeter/lego/types.py
+++ gsheeter/lego/types.py
@@ -14,5 +14,5 @@ DATETIME_TYPES = (
 
 DATETIME_REGEX = re.compile(r'^\d{2,4}[\-\/]{0,1}\d{2,4}[\-\/]{0,1}\d{2,4}.*\d{0,2}\:{0,1}\d{0,2}\:{0,1}\d{0,2}$')
 DIGIT_REGEX = re.compile(r'^\d+$')
-NUM_REGEX = re.compile(r'^\-{0,1}[0-9\,]+.{0,1}[0-9]*$')
+NUM_REGEX = re.compile(r'^-?[\d,]+\.{0,1}[\d]*$')
 PERC_REGEX = re.compile(r'^[\d\,]+\.{0,1}\d*\%{1}$')
